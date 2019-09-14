93fd8da5bc2e7abd9d9ca6363fe31d057bdddfbb
juansu4rez <jnssuarez@gmail.com>
2019-09-14 15:11:20 +1200

    tankBehaviour modified.

--- Assets/Scripts/Classes/TankBehavior.cs
+++ Assets/Scripts/Classes/TankBehavior.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class TankBehavior : MonoBehaviour
 {
+    Rigidbody rigidbody;
+    float movementSpeed = 2f;
     // Start is called before the first frame update
+
+    float smooth = 5.0f;
+    float tiltAngle = 45.0f;
+    int angle = 0;
+    int cooldown = 1;
+    float lastTime = 0;
+
     void Start()
     {
-        
+        rigidbody = transform.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Time.time > lastTime + cooldown)
+        {
+
+        //if (Input.GetKeyDown(KeyCode.UpArrow))
+        //rigidbody.transform.localRotation
+
+        //if (Input.GetKeyDown(KeyCode.DownArrow))
+
+        // Smoothly tilts a transform towards a target rotation.
+        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+        float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+
+        angle = (angle+90)% 360;
+
+        Quaternion target = Quaternion.Euler(0, angle, 0);
+
+        // Dampen towards the target rotation
+        //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+        transform.rotation = target;
+
         
+
+        lastTime = Time.time;
+        }
+
     }
 }
