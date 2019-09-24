4ea0a7cceb9c03dc552c0cf958c9fac027545e00
avargas_ind <avargas_ind@movilway.com>
2019-09-23 21:39:17 -0500

    TankBehavior updated

--- Assets/Scripts/Classes/TankBehavior.cs
+++ Assets/Scripts/Classes/TankBehavior.cs
@@ -5,46 +5,124 @@ using UnityEngine;
 public class TankBehavior : MonoBehaviour
 {
     Rigidbody rigidbody;
-    float movementSpeed = 2f;
-    // Start is called before the first frame update
+    public float movementSpeed = 20f;
 
-    float smooth = 5.0f;
+    public bool invertupdowndir = false;
+
+    bool IsButtonMovingPressed = false;
+
     float tiltAngle = 45.0f;
     int angle = 0;
     int cooldown = 1;
     float lastTime = 0;
 
+    private Vector3 centerofmass;
+
     void Start()
     {
+
         rigidbody = transform.GetComponent<Rigidbody>();
+
+        centerofmass = rigidbody.centerOfMass;
+    }
+
+    private void FixedUpdate()
+    {
+
+      
+        rigidbody.centerOfMass = centerofmass;
+    
+        if (IsButtonMovingPressed) {
+
+
+            // Bit shift the index of the layer (8) to get a bit mask
+            int layerMask = 0 << 8;
+
+            // This would cast rays only against colliders in layer 8.
+            // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+            layerMask = ~layerMask;
+
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            Quaternion qua = this.transform.rotation;
+            if (Physics.OverlapBox((this.transform.position + this.transform.forward * this.transform.localScale.z /2)  + ((this.transform.forward  ) * movementSpeed * Time.fixedDeltaTime), this.transform.localScale / 2, qua,0).Length> 0)
+            {
+                //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                //Debug.Log("Did Hit");
+                return;
+            }
+
+
+            this.rigidbody.MovePosition(this.transform.position + (this.transform.forward) * movementSpeed * Time.fixedDeltaTime);
+
+
+
+        }
+
+
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.time > lastTime + cooldown)
+        IsButtonMovingPressed = Input.GetKey(KeyCode.Space);
+
+
+
+        var yangle = -1;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+          if(!invertupdowndir)
+            yangle = 0;
+          else
+                yangle = 180;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (!invertupdowndir)
+                yangle = 180;
+            else
+                yangle = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            yangle = 270;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+               
+            yangle = 90;
+        }
+
+
+    
+
+        if (yangle > -1)
+        {
+            Quaternion target = Quaternion.Euler(0, yangle, 0);
+
+            // Dampen towards the target rotation
+            //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+            transform.rotation = target;
+        }
+
+        if (false && Time.time > lastTime + cooldown)
         {
 
-        //if (Input.GetKeyDown(KeyCode.UpArrow))
-        //rigidbody.transform.localRotation
 
-        //if (Input.GetKeyDown(KeyCode.DownArrow))
+            angle = (angle + 90) % 360;
 
-        // Smoothly tilts a transform towards a target rotation.
-        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
-        float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+            Quaternion target = Quaternion.Euler(0, angle, 0);
 
-        angle = (angle+90)% 360;
+            // Dampen towards the target rotation
+            //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+            transform.rotation = target;
 
-        Quaternion target = Quaternion.Euler(0, angle, 0);
 
-        // Dampen towards the target rotation
-        //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
-        transform.rotation = target;
 
-        
-
-        lastTime = Time.time;
+            lastTime = Time.time;
         }
 
     }
---
+++ Assets/Util/ObjectData.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectData : MonoBehaviour
+{
+
+    public int scale = 5;
+    //public bool showdirectionsongame = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.red;
+        var vectoraux = this.transform.position;
+        vectoraux.y = vectoraux.y - vectoraux.y;
+
+        Gizmos.DrawRay(vectoraux, ( transform.forward * scale));
+
+        Gizmos.color = Color.blue;
+    
+
+        Gizmos.DrawRay(vectoraux, (transform.right * scale));
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(vectoraux, (transform.up * scale));
+
+
+    }
+}
